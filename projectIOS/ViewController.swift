aa6fd4bb4e0bc68d8cbb8a21c6f7e4b922bb8d52
thomasgerand <42136569+thomasgerand@users.noreply.github.com>
2018-08-06 10:37:45 +0200

    Add files via upload

---
+++ projectIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectIOS
+//
+//  Created by Oleg GORBATCHEV on 16/07/2018.
+//  Copyright © 2018 gayopilic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
